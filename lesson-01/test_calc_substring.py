38a972c6bb424595b8e197ebef0712ea713d941c
g.kandaurov <g.kandaurov@corp.mail.ru>
2026-09-04 20:37:49 +0300

    add pipeline

--- lesson-01/test_calc_substring.py
+++ lesson-01/test_calc_substring.py
@@ -33,5 +33,4 @@ class TestIsSubstring(TestCase):
         self.assertFalse(is_substring("fff", "ff"))
         self.assertTrue(is_substring("fff", "ffff"))
         self.assertTrue(is_substring("F", "fff"))
-
-
+        self.assertTrue(is_substring("D", "fff"))
